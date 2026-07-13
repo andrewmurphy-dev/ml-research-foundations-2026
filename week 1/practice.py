eb85c853a16e07abd69ad0bbee4e5727091d0d7b
Andrew Murphy <legionastartesofficial@gmail.com>
2026-07-14 02:13:43 +0900

    day 1 , numpy

---
+++ week 1/practice.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+numbers = np.array([10, 50, 75, 23])
+
+
+print(numbers)
+print(type(numbers))
+
+
+#remmeber this 
+
+print(numbers.shape)
+print(numbers.ndim)
+print(numbers.dtype)
+print(numbers.size)
+
+

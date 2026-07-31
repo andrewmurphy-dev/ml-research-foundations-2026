e7b99f65f42d17c04991158c4b289127d0dff04d
Andrew Murphy <legionastartesofficial@gmail.com>
2026-08-01 05:18:40 +0900

    day 1 week 3

---
+++ week 3/topic_1.py
@@ -0,0 +1,39 @@
+#what is a vector?
+
+
+#A vector is an ordered collection of numbers.
+
+
+#for example
+
+
+import numpy as np
+
+import numpy as np
+
+patient = np.array([29, 120, 72])
+
+
+print(patient)
+
+
+#this vector contains three numbers 
+#[ 29 120  72]
+
+#So one vector can represent one patient with three features.
+
+print(patient.shape)
+#(3,)
+#3 values
+#1 dimension
+#The comma is part of how Python represents a one-dimensional shape.
+
+
+
+
+
+
+#[29, 120, 72]                → vector
+
+#[[29, 120, 72],
+# [45, 135, 80]]              → matrix

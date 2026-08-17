410004307bf443793e253b39945f2c7ce302f86e
Andrew Murphy <legionastartesofficial@gmail.com>
2026-08-17 22:43:29 +0900

    day 6 week 4

---
+++ week 4/day 6/topic_2.py
@@ -0,0 +1,16 @@
+#Correlation Does Not Mean Causation
+
+#what is causation 
+
+#one thing actually causes another thing to happen.
+
+#correlation
+#= two things change together
+
+#causation
+#= one thing produces the change in the other
+
+
+
+#causation
+#= one variable actually causes a change in the other
